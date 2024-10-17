ef961dd205852d0109b08b51b21d2ff84cba4c56
Thang7749 <thangnq5089@ut.edu.vn>
2024-10-17 20:47:21 +0700

    test

---
+++ KOI_POND/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using KOI_POND.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KOI_POND.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
